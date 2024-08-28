b2d930b5218c00b1a0403627a26c4df1e505e429
mbanze2936 <mbanze2936@gmail.com>
2024-08-28 23:01:42 +0530

    edits

--- model_generator.py
+++ model_generator.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, create_model
 from typing import Dict, Type
 
 type_mapping = {
-    "STRING": str,
     "NUMBER": int,
-    "BOOLEAN": bool,
-    "FLOAT": float
+    "FLOAT": float,
+    "STRING": str,
+    "BOOLEAN": bool
 }
 
 
